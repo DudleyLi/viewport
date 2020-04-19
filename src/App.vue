c95c7031fbf2e425df41af3536b6d0a46da933b6
lidongli <1344066958@qq.com>
2020-04-19 12:10:32 +0800

    ’m初始化项目

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+     <div class="div1">哈哈哈</div>
+     <div class="div2">嘻嘻</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang='scss'>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  .div1 {
+    width:  200rem/$e;
+    height: 100rem/$e;
+    background-color: $theme-color;
+    font-size: 32rem/$e;
+  }
+  .div2{
+    width:  400rem/$e;
+    height: 100rem/$e;
+    background-color:red;
+    font-size: 32rem/$e;
+  }
+}
+
+</style>
